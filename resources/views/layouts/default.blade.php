6fe7e9b45fb6781a48bf9cdbf60069c351b865e8
zgy0777 <soldier76@qq.com>
2018-11-08 12:56:03 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title','Sample')</title>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    {{--引入public对应的css--}}
+    {{--本质上是sass先编译再写入到app.css中--}}
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
+{{--头部--}}
+@include('layouts._header')
+
+<div class="container">
     @yield('content')
+
+    {{--脚部--}}
+    @include('layouts._footer')
+
+</div>
+
+
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 //首页
-Route::get('/','StaticPagesController@home');
+Route::get('/','StaticPagesController@home')->name('home');
 //帮助页
-Route::get('/help','StaticPagesController@help');
+Route::get('/help','StaticPagesController@help')->name('help');
 //关于页
-Route::get('/about','StaticPagesController@about');
+Route::get('/about','StaticPagesController@about')->name('about');
+
+//注册
+Route::get('signup', 'UsersController@create')->name('signup');
